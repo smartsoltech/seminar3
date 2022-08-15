168677c00983310775788b63f994f9ecf49de371
Andrew Choi <a.choi@smartsoltech.kr>
2022-08-15 19:12:37 +0900

    Главный коммит. псевдокод из блок-схемы

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print(f'Hello world!')
+array = [2, 5, 13, 7, 6, 4]
+size = 6
+sum = 0
+avg = 0
+index = 0
+while index < size:
+    sum = sum+array[index]
+    index = index+1
+avg = sum/size
+print (avg)
