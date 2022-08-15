2112288719bfaa6e13acee2b38cf86e3e52ad665
Andrew Choi <a.choi@smartsoltech.kr>
2022-08-15 20:50:07 +0900

    ТУЦ ТУЦ ТУЦ

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ sum = 0
 avg = 0
 index = 0
 for item in range(int(size)):
-    array.append(input(f'Введите {item} элемент массива: '))
+    array.append(input(f'Введите {item+1} элемент массива: '))
 print(f'Массив заполнен: {array}')
 while index < int(size):
     sum = int(sum)+int(array[index])
     index = index+1
 avg = sum/size
-print (avg)
+print (f'Арифметическое среднее значение массива {array} равняется {avg}')
